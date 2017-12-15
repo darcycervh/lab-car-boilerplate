d4e4c3a80e7ea6171b3886bd83b809910ca01b6d
darcycervh <darcycervera@gmail.com>
2017-12-15 13:02:02 -0500

    avanzando maquetación

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 // La funcionalidad de tu proyecto
 function initMap() {
-    var uluru = {lat: -25.363, lng: 131.044};
+    var uluru = {lat: -12.120290210463269, lng: -76.67679206875005};
     var map = new google.maps.Map(document.getElementById('map'), {
       zoom: 4,
       center: uluru
